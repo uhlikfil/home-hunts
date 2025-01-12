6e04e789f490d6ea5200ff0bb150a29646b20553
Filip Uhlík <filipfilauhlik@gmail.com>
2025-01-12 20:03:33 +0100

    feat: Working Password Lock

--- clueapp/widgets/lock.py
+++ clueapp/widgets/lock.py
@@ -1,6 +1,11 @@
+import logging
+
+import textual as tx
 import textual.app as txa
 import textual.reactive as txr
 import textual.widgets as txw
+
+logger = logging.getLogger(__name__)
 
 
 class Lock(txw.Static):
@@ -12,28 +17,29 @@ class UnlockedLock(Lock):
 
 
 class PasswordLock(Lock):
-    def __init__(
-        self,
-        password: str,
-        *,
-        expand: bool = False,
-        shrink: bool = False,
-        markup: bool = True,
-        name: str | None = None,
-        id: str | None = None,
-        classes: str | None = None,
-        disabled: bool = False,
-    ):
-        super().__init__(
-            expand=expand,
-            shrink=shrink,
-            markup=markup,
-            name=name,
-            id=id,
-            classes=classes,
-            disabled=disabled,
-        )
+    show_invalid_pwd_msg = txr.reactive(False)
+
+    def __init__(self, password: str):
+        super().__init__()
         self.password = password
 
     def compose(self) -> txa.ComposeResult:
-        yield txw.Input(placeholder="enter password")
+        yield txw.Input(placeholder="enter password", password=True)
+
+    def watch_show_invalid_pwd_msg(self, show: bool) -> None:
+        logger.info(f"show invalid password message {show=}")
+        if show:
+            self.mount(txw.Label("Invalid password", id="invalid-pwd-msg"))
+        else:
+            self.query("#invalid-pwd-msg").remove()
+
+    @tx.on(txw.Input.Submitted)
+    def enter_password(self, event: txw.Input.Submitted) -> None:
+        if event.value == self.password:
+            logger.info("correct password entered")
+            self.is_locked = False
+            self.show_invalid_pwd_msg = False
+        else:
+            logger.info("invalid password entered")
+            self.show_invalid_pwd_msg = True
+        event.input.value = ""
